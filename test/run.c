48058660fa2e132d48cc8bb5fe2ea6be44a30d96
meh <meh@schizofreni.co>
2012-12-25 21:35:33 +0100

    test: remove executable and update test for siphash

--- test/run.c
+++ test/run.c
@@ -4,10 +4,18 @@
 int
 main (int argc, char* argv[])
 {
-	uint8_t key[16] = { 0 };
-	uint8_t data[20] = { 1 };
+	{
+		uint8_t    key[16]  = {0};
+		uint8_t    data[20] = {0};
+		siphash_t* sip      = siphash_new();
 
-	printf("%lu\n", siphash(key, data, 20));
+		siphash_init_default(sip, key);
+		siphash_update(sip, data, 20);
+		siphash_finalize(sip);
+
+		printf("siphash:             %lu\n", siphash(key, data, 20));
+		printf("siphash (streaming): %lu\n", siphash_fetch(sip));
+	}
 
 	return 0;
 }
