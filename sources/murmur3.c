668bb69ea8b3bd4490e406be49d5314c5a9c8a14
meh <meh@schizofreni.co>
2012-12-26 22:15:04 +0100

    murmur3: improve one pass implementation

--- sources/murmur3.c
+++ sources/murmur3.c
@@ -48,15 +48,13 @@ murmur3 (hash_t seed, void* buffer, size_t length)
 	assert(buffer);
 
 	#if HASH_BIT == 32
-		uint8_t* buf    = buffer;
-		int      number = length / 4;
-		uint32_t h1     = seed;
-		uint32_t c1     = 0xCC9E2D51;
-		uint32_t c2     = 0x1B873593;
+		uint32_t h1 = seed;
+		uint32_t c1 = 0xCC9E2D51;
+		uint32_t c2 = 0x1B873593;
 
-		uint32_t* blocks = (uint32_t*) (buf + (number * 4));
-		for (int i = -number; i < 0; i++) {
-			uint32_t k1 = GET32(blocks, i);
+		size_t i, blocks;
+		for (i = 0, blocks = length & ~3; i < blocks; i += 4) {
+			uint32_t k1 = GET32(buffer, i);
 
 			k1 *= c1; k1 = ROTL32(k1, 15); k1 *= c2;
 			h1 ^= k1; h1 = ROTL32(h1, 13);
@@ -64,39 +62,45 @@ murmur3 (hash_t seed, void* buffer, size_t length)
 			h1 = 0xE6546B64 + (h1 * 5);
 		}
 
+		#if __BYTE_ORDER_ == __ORDER_LITTLE_ENDIAN
+			#define INDEX_FOR(n) n
+		#elif __BYTE_ORDER__ == __ORDER_BIG__ENDIAN__
+			#define INDEX_FOR(n) -(n - 2)
+		#else
+			#error "I don't see any gigantic endian."
+		#endif
 		{
-			uint8_t* tail = buf + (number * 4);
-			uint32_t k1   = 0;
+			uint8_t* buf = buffer;
+			uint32_t k1  = 0;
 
-			switch (length & 3) {
-				case 3: k1 ^= tail[2] << 16;
-				case 2: k1 ^= tail[1] << 8;
+			switch (length - blocks) {
+				case 3: k1 ^= buf[i + INDEX_FOR(2)] << 16;
+				case 2: k1 ^= buf[i + INDEX_FOR(1)] << 8;
 
 				case 1:
-					k1 ^= tail[0];
+					k1 ^= buf[i + INDEX_FOR(0)];
 					k1 *= c1;
-					k1  = ROTL32(k1,15);
+					k1  = ROTL32(k1, 15);
 					k1 *= c2;
 					h1 ^= k1;
 			}
 		}
+		#undef INDEX_FOR
 
 		h1 ^= length;
 		h1  = fmix(h1);
 
 		return h1;
 	#elif HASH_BIT == 64
-		uint8_t* buf    = buffer;
-		int      number = length / 16;
-		uint64_t h1     = seed;
-		uint64_t h2     = seed;
-		uint64_t c1     = 0x87C37B91114253D5ull;
-		uint64_t c2     = 0x4CF5AD432745937Full;
+		uint64_t h1 = seed;
+		uint64_t h2 = seed;
+		uint64_t c1 = 0x87C37B91114253D5ull;
+		uint64_t c2 = 0x4CF5AD432745937Full;
 
-		uint64_t* blocks = (uint64_t*) buf;
-		for (int i = 0; i < number; i++) {
-			uint64_t k1 = GET64(blocks, i * 2);
-			uint64_t k2 = GET64(blocks, i * 2 + 1);
+		size_t i, blocks;
+		for (i = 0, blocks = length & ~15; i < blocks; i += 16) {
+			uint64_t k1 = GET64(buffer, i);
+			uint64_t k2 = GET64(buffer, i + 8);
 
 			k1 *= c1; k1 = ROTL64(k1, 31); k1 *= c2;
 			h1 ^= k1; h1 = ROTL64(h1, 27); h1 += h2;
@@ -109,42 +113,50 @@ murmur3 (hash_t seed, void* buffer, size_t length)
 			h2 = 0x38495AB5 + h2 * 5;
 		}
 
+		#if __BYTE_ORDER_ == __ORDER_LITTLE_ENDIAN
+			#define INDEX_FOR(n) n
+		#elif __BYTE_ORDER__ == __ORDER_BIG__ENDIAN__
+			#define INDEX_FOR(n) -(n - 16)
+		#else
+			#error "I don't know the size of this endian"
+		#endif
 		{
-			uint8_t* tail = buf + number * 16;
-			uint64_t k1   = 0;
-			uint64_t k2   = 0;
+			uint8_t* buf = buffer;
+			uint64_t k1  = 0;
+			uint64_t k2  = 0;
 
-			switch (length & 15) {
-				case 15: k2 ^= (uint64_t) tail[14] << 48;
-				case 14: k2 ^= (uint64_t) tail[13] << 40;
-				case 13: k2 ^= (uint64_t) tail[12] << 32;
-				case 12: k2 ^= (uint64_t) tail[11] << 24;
-				case 11: k2 ^= (uint64_t) tail[10] << 16;
-				case 10: k2 ^= (uint64_t) tail[9]  << 8;
+			switch (length - blocks) {
+				case 15: k2 ^= (uint64_t) buf[i + INDEX_FOR(14)] << 48;
+				case 14: k2 ^= (uint64_t) buf[i + INDEX_FOR(13)] << 40;
+				case 13: k2 ^= (uint64_t) buf[i + INDEX_FOR(12)] << 32;
+				case 12: k2 ^= (uint64_t) buf[i + INDEX_FOR(11)] << 24;
+				case 11: k2 ^= (uint64_t) buf[i + INDEX_FOR(10)] << 16;
+				case 10: k2 ^= (uint64_t) buf[i + INDEX_FOR(9)]  << 8;
 
 				case 9:
-					k2 ^= (uint64_t) tail[8];
+					k2 ^= (uint64_t) buf[i + INDEX_FOR(8)];
 					k2 *= c2;
 					k2  = ROTL64(k2, 33);
 					k2 *= c1;
 					h2 ^= k2;
 
-				case 8: k1 ^= (uint64_t) tail[7] << 56;
-				case 7: k1 ^= (uint64_t) tail[6] << 48;
-				case 6: k1 ^= (uint64_t) tail[5] << 40;
-				case 5: k1 ^= (uint64_t) tail[4] << 32;
-				case 4: k1 ^= (uint64_t) tail[3] << 24;
-				case 3: k1 ^= (uint64_t) tail[2] << 16;
-				case 2: k1 ^= (uint64_t) tail[1] << 8;
+				case 8: k1 ^= (uint64_t) buf[i + INDEX_FOR(7)] << 56;
+				case 7: k1 ^= (uint64_t) buf[i + INDEX_FOR(6)] << 48;
+				case 6: k1 ^= (uint64_t) buf[i + INDEX_FOR(5)] << 40;
+				case 5: k1 ^= (uint64_t) buf[i + INDEX_FOR(4)] << 32;
+				case 4: k1 ^= (uint64_t) buf[i + INDEX_FOR(3)] << 24;
+				case 3: k1 ^= (uint64_t) buf[i + INDEX_FOR(2)] << 16;
+				case 2: k1 ^= (uint64_t) buf[i + INDEX_FOR(1)] << 8;
 
 				case 1:
-					k1 ^= (uint64_t) tail[0];
+					k1 ^= (uint64_t) buf[i + INDEX_FOR(0)];
 					k1 *= c1;
 					k1  = ROTL64(k1, 31);
 					k1 *= c2;
 					h1 ^= k1;
 			}
 		}
+		#undef INDEX_FOR
 
 		h1 ^= length;
 		h2 ^= length;
