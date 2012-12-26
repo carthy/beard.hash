f076df8785f3b06ccbbb47713e184cac14059531
meh <meh@schizofreni.co>
2012-12-26 23:04:18 +0100

    test: add murmur3 test

--- test/run.c
+++ test/run.c
@@ -19,5 +19,20 @@ main (int argc, char* argv[])
 		puts("");
 	}
 
+	{
+		hash_t     seed     = 0;
+		uint8_t    data[20] = {0};
+		murmur3_t* mur      = murmur3_new();
+
+		murmur3_init(mur, seed);
+		murmur3_update(mur, data, 10);
+		murmur3_update(mur, data + 10, 10);
+		murmur3_final(mur);
+
+		printf("murmur3:             %lu\n", murmur3(seed, data, 20));
+		printf("murmur3 (streaming): %lu\n", murmur3_fetch(mur));
+		puts("");
+	}
+
 	return 0;
 }
